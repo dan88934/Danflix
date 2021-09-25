2383c6ced27681c84087e54650fbaf4777529f51
Daniel Hughes <Daniel@Daniels-MacBook-Pro-7.local>
2021-09-25 17:19:19 +0100

    Danflix logo now appearing on page

--- react_frontend/src/containers/header.js
+++ react_frontend/src/containers/header.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Header } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.png'
-
 export function HeaderContainer({ children }) {
     return (
         <Header>
             <Header.Frame>
-               <Header.Logo to={ROUTES.HOME} alt="Neflix" src={logo}/>
+               <Header.Logo to={ROUTES.HOME} alt="Neflix" src= {require(.../lib/images/logo.png)}/>
                <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink> 
             </Header.Frame>
             {children}
